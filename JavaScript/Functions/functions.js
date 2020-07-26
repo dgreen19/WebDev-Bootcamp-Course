a4ada397844567cc1b2d9cdc85f2caef3f7a3eaa
Darryl <greenjr91@gmail.com>
2020-07-25 21:03:44 -0400

    07/25 update

---
+++ JavaScript/Functions/functions.js
@@ -0,0 +1,24 @@
+alert('CONNECTED!');
+
+function singSong() {
+	console.log('Twinkle, twinkle, little star,');
+	console.log('How I wonder what you are!');
+	console.log('Up above the world so high,');
+	console.log('Like a diamond in the sky.');
+	console.log('When the blazing sun is gone,');
+	console.log('When he nothing shines upon,');
+	console.log('Then you show your little light,');
+	console.log('Twinkle, twinkle, all the night.');
+	console.log('Twinkle, twinkle, little star,');
+	console.log('How I wonder what you are!');
+}
+
+function square(num) {
+	console.log(num * num);
+}
+
+function helloThere() {
+	// body...
+	console.log('Hello There!');
+	console.log('General Kenobi! You are a bold one!');
+}
